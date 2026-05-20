17cd1ef5b5466e8d5eaa1f56045372e06295fa04
kyonRay <rui.kwok@outlook.com>
2026-05-20 19:28:07 +0800

    chore: add eslint flat config so pnpm lint can run

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      "src-tauri/target/",
+      "**/*.d.ts",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
